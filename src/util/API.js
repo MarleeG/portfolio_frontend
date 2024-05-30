4b6dc3d73316e186f9656bfdc06e2d374c72bbe8
Marlee Gerard <marleegerard@gmail.com>
2024-05-30 00:13:44 -0400

    added new proxy

--- src/util/API.js
+++ src/util/API.js
@@ -1,19 +1,17 @@
 import axios from "axios";
 
-const API_URL = 'http://ec2-3-80-114-117.compute-1.amazonaws.com:8080';
-
 const API = {
   getAllProjects: () => {
-    return axios.get(`${API_URL}/api/project/all`);
+    return axios.get(`/api/project/all`);
   },
   getProjectById: (id) => {
-    return axios.get(`${API_URL}/api/project/${id}`);
+    return axios.get(`/api/project/${id}`);
   },
   getAllProjectImageS3Urls: () => {
-    return axios.get(`${API_URL}/api/project/images`);
+    return axios.get(`/api/project/images`);
   },
   getAllConnectIconImageS3Urls: () => {
-    return axios.get(`${API_URL}/api/project/connect/images`)
+    return axios.get(`/api/project/connect/images`)
   }
 };
 
